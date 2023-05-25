61505ba9375e9ee89cd91498faea31ee6232a6aa
srua <srua029@misena.edu.co>
2023-05-24 19:17:33 -0500

    aa

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -7,25 +7,26 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.checkLocalStorage();
   }
 
   checkLocalStorage() {
-    if(!localStorage.getItem('token')){
+    if (!localStorage.getItem('token')) {
       this.router.navigate(['login']);
     }
   }
 
   modules = [
-    { name: 'Roles', route: '/list-roles'},
-    { name: 'Modulos', route: '/list-modulos'},
-    { name: 'Permisos', route: '/list-permisos'},
-    { name: 'Clientes', route: '/list-clientes'},
-    { name: 'Usuarios', route: '/list-usuarios'},
+    { name: 'Roles', route: '/list-roles' },
+    { name: 'Modulos', route: '/list-modulos' },
+    { name: 'Permisos', route: '/list-permisos' },
+    { name: 'Clientes', route: '/list-clientes' },
+    { name: 'Usuarios', route: '/list-usuarios' },
+    { name: 'Paquetes', route: '/list-paquetes' }
   ]
 }
